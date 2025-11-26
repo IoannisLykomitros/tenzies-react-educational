ce902250885a595769c3d03a79190c9665ca4660
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-26 16:20:58 +0200

    added confetti when user wins the game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,30 @@
 import { nanoid } from "nanoid"
 import Die from "./Die"
 import { useState } from "react"
+import { useWindowSize } from 'react-use'
+import ReactConfetti from "react-confetti"
 
 const App = () => {
     const [dice, setDice] = useState(generateAllNewDice())
+
+    let gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => ({
                 value: Math.ceil(Math.random() * 6), 
-                isHeld: true,
+                isHeld: false,
                 id: nanoid()
             }))
     }
 
     const rollDice = () => {
-        setDice(generateAllNewDice())
+        setDice(oldDice => oldDice.map(die => 
+            die.isHeld ?
+                die :
+                { ...die, value: Math.ceil(Math.random() * 6) }
+        ))
     }
 
     const hold = (id) => {
@@ -28,14 +36,22 @@ const App = () => {
     }
 
     const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} hold={() => hold(dieObj.id)} />)
+    const { width, height } = useWindowSize()
 
     return (
         <main>
+            {gameWon && <ReactConfetti width={width} height={height} />}
+            <div>
+                <h1 className="title">Tenzies</h1>
+                <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            </div>
             <div className="dice-container">
                 {diceElements}
             </div>
 
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
