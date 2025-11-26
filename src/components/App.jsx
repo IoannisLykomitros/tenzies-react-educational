1531bf3f402f2d7a70f88205b2e401a661a2d467
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-26 17:17:10 +0200

    added logic so the user can start new game

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { useWindowSize } from 'react-use'
 import ReactConfetti from "react-confetti"
 
 const App = () => {
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(() => generateAllNewDice())
 
     let gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
@@ -20,11 +20,15 @@ const App = () => {
     }
 
     const rollDice = () => {
-        setDice(oldDice => oldDice.map(die => 
-            die.isHeld ?
-                die :
-                { ...die, value: Math.ceil(Math.random() * 6) }
-        ))
+        if (!gameWon) {
+            setDice(oldDice => oldDice.map(die =>
+                die.isHeld ?
+                    die :
+                    { ...die, value: Math.ceil(Math.random() * 6) }
+            ))
+        } else {
+            setDice(generateAllNewDice())
+        }
     }
 
     const hold = (id) => {
